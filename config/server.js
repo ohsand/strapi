22511446d72806b315dd0ee38bda95a295b24f59
ohsand <osand.business@gmail.com>
2024-04-12 15:05:51 +0200

    update variables

--- config/server.js
+++ config/server.js
@@ -1,17 +1,8 @@
-const crypto = require('crypto');
-
-// Generate random keys
-const key1 = crypto.randomBytes(32).toString('hex');
-const key2 = crypto.randomBytes(32).toString('hex');
-
-console.log("Key 1:", key1);
-console.log("Key 2:", key2);
-
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: [key1, key2], // Assign keys as an array directly
+    keys: env('APP_KEYS', '').split(','), // Split comma-separated keys
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
