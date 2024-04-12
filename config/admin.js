b7d952cddc179b78f31661bebcdf0da77c6bb318
ohsand <osand.business@gmail.com>
2024-04-12 15:40:05 +0200

    update values

--- config/admin.js
+++ config/admin.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('STRAPI_TRANSFER_TOKEN_SALT', 'defaultTransferSaltValue'),
     },
   },
   flags: {
